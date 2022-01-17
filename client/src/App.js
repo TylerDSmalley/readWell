a280dda598b9fc8b3d38b0073b678d05e0cdfa95
Tyler <TylerDSmalley@gmail.com>
2022-01-17 13:38:00 -0500

    Update App.js
    added missing imports to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,47 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+//import Home from './pages/Home';
+import Registration from "./pages/Registration";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import { AuthContext } from "./helpers/AuthContext";
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
 
 function App() {
+  const [authState, setAuthState] = useState({
+    username: "",
+    id: 0,
+    status: false,
+  });
+
+  useEffect((authState) => {
+    axios
+      .get("http://localhost:3001/auth/auth", {
+        headers: {
+          accessToken: localStorage.getItem("accessToken"),
+        },
+      })
+      .then((response) => {
+        if (response.data.error) {
+          setAuthState({ ...authState, status: false });
+        } else {
+          setAuthState({
+            username: response.data.username,
+            id: response.data.id,
+            status: true,
+          });
+        }
+      });
+  }, []);
+
+  const logout = () => {
+    localStorage.removeItem("accessToken");
+    setAuthState({ username: "", id: 0, status: false });
+  };
+
   return (
     <div className="App">
       <AuthContext.Provider value={{ authState, setAuthState }}>
@@ -14,7 +55,7 @@ function App() {
                 </>
               ) : (
                 <>
-                  <Link to="/"> Home Page</Link>
+                  {/* {<Link to="/"> Home Page</Link>} */}
                 </>
               )}
             </div>
@@ -24,11 +65,14 @@ function App() {
             </div>
           </div>
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/book/:id' element={<Post />} />
             <Route path='/registration' element={<Registration />} />
             <Route path='/login' element={<Login />} />
+            {/*
+            <Route path='/' element={<Home />} /> 
+            <Route path='/book/:id' element={<Post />} />
+            
             <Route path="/profile/:id" element={<Profile />} />
+            */}
             <Route path='/*' element={<PageNotFound />} />
           </Routes>
         </Router>
