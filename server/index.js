d44e2ad70b500892dad36d2d843e737fae374d2b
Tyler <TylerDSmalley@gmail.com>
2022-01-17 13:37:45 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,6 @@ const db = require("./models");
 const usersRouter = require("./routes/Users")
 app.use("/auth", usersRouter);
 
-
 db.sequelize.sync().then(() => {
     app.listen(3001, () => {
     console.log("Server running on port 3001");
