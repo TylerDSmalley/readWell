c65de006da3f98da1e6fccb51ea4f976d3546039
Tyler <TylerDSmalley@gmail.com>
2022-01-17 19:00:18 -0500

    Batch updated
    Added material UI components to log in

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,44 +2,131 @@ import React, { useState, useContext } from 'react'
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../helpers/AuthContext";
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const {setAuthState} = useContext(AuthContext)
+    const { setAuthState } = useContext(AuthContext)
     let navigate = useNavigate();
 
     const login = () => {
         const data = { email: email, password: password };
         axios.post("http://localhost:3001/auth/login", data).then((response) => {
-            if(response.data.error) {
+            if (response.data.error) {
                 alert(response.data.error);
             } else {
-            localStorage.setItem("accessToken", response.data.token);
-            setAuthState({ email: response.data.email, id: response.data.id, status: true});
-            navigate("/");
+                localStorage.setItem("accessToken", response.data.token);
+                setAuthState({ email: response.data.email, id: response.data.id, status: true });
+                navigate("/");
             };
         });
     };
 
-    return (
-        <div className="loginContainer">
-            <label>email:</label>
-            <input
-                type="text"
-                onChange={(event) => {
-                    setEmail(event.target.value);
-                }}
-            />
-            <label>Password:</label>
-            <input
-                type="password"
-                onChange={(event) => {
-                    setPassword(event.target.value);
-                }}
-            />
+    function Copyright(props) {
+        return (
+            <Typography variant="body2" color="text.secondary" align="center" {...props}>
+                {'Copyright © '}
+                <Link color="inherit" href="#">
+                    Team IT
+                </Link>{' '}
+                {new Date().getFullYear()}
+                {'.'}
+            </Typography>
+        );
+    };
 
-            <button onClick={login}> Login </button>
+    const theme = createTheme();
+
+    return (
+        <div >
+            <ThemeProvider theme={theme}>
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Sign in
+                        </Typography>
+                        <Box component="form" onSubmit={login} noValidate sx={{ mt: 1 }}>
+                            <TextField
+                                className="bg-light"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                                onChange={(event) => {
+                                    setEmail(event.target.value);
+                                }}
+                            />
+                            <TextField
+                                className="bg-light"
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                                onChange={(event) => {
+                                    setPassword(event.target.value);
+                                }}
+                            />
+                            <FormControlLabel
+                                control={<Checkbox value="remember" color="primary" />}
+                                label="Remember me"
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Sign In
+                            </Button>
+                            <Grid container>
+                                <Grid item xs>
+                                    <Link href="#" variant="body2">
+                                        Forgot password?
+                                    </Link>
+                                </Grid>
+                                <Grid item>
+                                    <Link href="#" variant="body2">
+                                        {"Don't have an account? Sign Up"}
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Box>
+                    <Copyright sx={{ mt: 8, mb: 4 }} />
+                </Container>
+            </ThemeProvider>
         </div>
     )
 }
