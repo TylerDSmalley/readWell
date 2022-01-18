6c5010e1a4e6e91b92a6eefa1b1d21a36696c580
Tyler <TylerDSmalley@gmail.com>
2022-01-18 18:44:21 -0500

    Create Books.js
    created Books routes
    -added CRUD routes to books

---
+++ server/routes/Books.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const router = express.Router();
+const { Books } = require("../models");
+const { validateToken } = require("../middlewares/AuthMiddleware");
+
+
+//BOOKS ROUTES-------------------------
+
+//add book
+router.post("/", validateToken, async (req, res) => {
+    console.log(req.body);
+    const { title, author, summary, genre, datePublished, publisher, isbn, coverPhoto } = req.body;
+    await Books.create({
+        title: title,
+        author: author,
+        summary: summary,
+        genre: genre,
+        datePublished: datePublished,
+        publisher: publisher,
+        isbn: isbn,
+        coverPhoto: coverPhoto,
+    });
+    res.json("BOOK ADDED TO LIBRARY");
+});
+
+//get all books
+router.get("/", validateToken, async (req, res) => {
+    const listOfBooks = await Posts.findAll({ include: [Reviews, Bookshelves] });
+    res.json({ listOfBooks: listOfBooks });
+});
+
+//update book by id
+router.put("/update/:bookId", async (req, res) => {
+    await Books.update({
+        title: req.body.title,
+        author: req.body.author,
+        summary: req.body.summary,
+        genre: req.body.genre,
+        datePublished: req.body.datePublished,
+        publisher: req.body.publisher,
+        isbn: req.body.isbn,
+        coverPhoto: req.body.coverPhoto,
+    }, { where: {id : req.body.id} });
+    res.json(req.body);
+});
+
+//delete book by id
+router.delete("delete/:bookId", validateToken, async (req, res) => {
+    const bookId = req.params.bookId;
+    await Books.destroy({
+        where: {
+            id: bookId,
+        },
+    });
+    res.json("BOOK DELETED");
+})
+
+module.exports = router;
