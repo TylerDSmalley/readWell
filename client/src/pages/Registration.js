3779b9a08255d940cca522c407ba8533aa0bf568
Tyler <TylerDSmalley@gmail.com>
2022-01-17 21:44:23 -0500

    Update Registration.js
    implemented material into registration page

--- client/src/pages/Registration.js
+++ client/src/pages/Registration.js
@@ -3,28 +3,63 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function Registration() {
 
     let navigate = useNavigate();
+    const theme = createTheme();
 
-    const initialValues = {
-        firstName: "",
-        lastName: "",
-        email: "",
-        password: "",
-        passwordConfirmation: "",
-    };
+    function Copyright(props) {
+        return (
+            <Typography variant="body2" color="text.secondary" align="center" {...props}>
+                {'Copyright © '}
+                <Link color="inherit" href="https://mui.com/">
+                    Your Website
+                </Link>{' '}
+                {new Date().getFullYear()}
+                {'.'}
+            </Typography>
+        );
+    }
 
-    const validationSchema = Yup.object().shape({
-        firstName: Yup.string().min(1).max(30).required("You must input a first name"),
-        lastName: Yup.string().min(1).max(30).required("You must input a last name"),
-        email: Yup.string().min(3).max(15).required("You must input a email"),
-        password: Yup.string().min(4).max(20).required("You must input a password"),
-        passwordConfirmation: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match')
-    });
+    // const initialValues = {
+    //     firstName: "",
+    //     lastName: "",
+    //     email: "",
+    //     password: "",
+    //     passwordConfirmation: "",
+    // };
 
-    const onSubmit = (data) => {
+    // const validationSchema = Yup.object().shape({
+    //     firstName: Yup.string().min(1).max(30).required("You must input a first name"),
+    //     lastName: Yup.string().min(1).max(30).required("You must input a last name"),
+    //     email: Yup.string().min(3).max(15).required("You must input a email"),
+    //     password: Yup.string().min(4).max(20).required("You must input a password"),
+    //     passwordConfirmation: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match')
+    // });
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const form = new FormData(event.currentTarget);
+        let data = {
+            firstName: form.get('firstName'),
+            lastName: form.get('lastName'),
+            email: form.get('email'),
+            password: form.get('password'),
+        };
         axios.post("http://localhost:3001/auth", data).then((response) => {
             navigate("/");
         });
@@ -32,50 +67,99 @@ function Registration() {
 
 
     return (
-        <div className="loginContainer">
-            <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
-                <Form className="formContainer">
-
-                    <label>First Name: </label>
-                    <ErrorMessage name="firstName" component="span" />
-                    <Field
-                        name="firstName"
-                        placeholder="(Ex. Jane...)"
-                    />
-
-                    <label>Last Name: </label>
-                    <ErrorMessage name="lastName" component="span" />
-                    <Field
-                        name="lastName"
-                        placeholder="(Ex. Doe...)"
-                    />
-
-                    <label>email: </label>
-                    <ErrorMessage name="email" component="span" />
-                    <Field
-                        name="email"
-                        placeholder="(Ex. John123...)"
-                    />
-
-                    <label>Password: </label>
-                    <ErrorMessage name="password" component="span" />
-                    <Field
-                        type="password"
-                        name="password"
-                        placeholder="(Ex. password...)"
-                    />
-
-                    <label>Confirm Password: </label>
-                    <ErrorMessage name="passwordConfirmation" component="span" />
-                    <Field
-                        type="password"
-                        name="passwordConfirmation"
-                        placeholder="(Ex. password...)"
-                    />
-
-                    <button type="submit"> Register </button>
-                </Form>
-            </Formik>
+        <div>
+            <ThemeProvider theme={theme}>
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Sign up
+                        </Typography>
+                        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField
+                                        className="bg-light"
+                                        autoComplete="given-name"
+                                        name="firstName"
+                                        required
+                                        fullWidth
+                                        id="firstName"
+                                        label="First Name"
+                                        autoFocus
+                                    />
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField
+                                        className="bg-light"
+                                        required
+                                        fullWidth
+                                        id="lastName"
+                                        label="Last Name"
+                                        name="lastName"
+                                        autoComplete="family-name"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        className="bg-light"
+                                        required
+                                        fullWidth
+                                        id="email"
+                                        label="Email Address"
+                                        name="email"
+                                        autoComplete="email"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        className="bg-light"
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        label="Password"
+                                        type="password"
+                                        id="password"
+                                        autoComplete="new-password"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <FormControlLabel
+                                        control={<Checkbox value="allowExtraEmails" color="primary" />}
+                                        label="I want to receive inspiration, marketing promotions and updates via email."
+                                    />
+                                </Grid>
+                            </Grid>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Sign Up
+                            </Button>
+                            <Grid container justifyContent="flex-end">
+                                <Grid item>
+                                    <Link href="/login" variant="body2">
+                                        Already have an account? Sign in
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Box>
+                    <Copyright sx={{ mt: 5 }} />
+                </Container>
+            </ThemeProvider>
         </div>
     )
 }
