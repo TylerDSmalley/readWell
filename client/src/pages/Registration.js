2ebfdc4636bfc1c2baab319794e8c0db3081a05e
Tyler <TylerDSmalley@gmail.com>
2022-01-17 23:05:19 -0500

    Update Registration.js
    reintegrated formik/yup validation into registration...I don't like it. I'll need to figure out another way to incorporate form validation while using material forms.

--- client/src/pages/Registration.js
+++ client/src/pages/Registration.js
@@ -6,9 +6,9 @@ import { useNavigate } from "react-router-dom";
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
+// import TextField from '@mui/material/TextField';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -16,6 +16,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 
 function Registration() {
 
@@ -26,8 +27,8 @@ function Registration() {
         return (
             <Typography variant="body2" color="text.secondary" align="center" {...props}>
                 {'Copyright © '}
-                <Link color="inherit" href="https://mui.com/">
-                    Your Website
+                <Link color="inherit" href="#">
+                    Team IT
                 </Link>{' '}
                 {new Date().getFullYear()}
                 {'.'}
@@ -35,23 +36,23 @@ function Registration() {
         );
     }
 
-    // const initialValues = {
-    //     firstName: "",
-    //     lastName: "",
-    //     email: "",
-    //     password: "",
-    //     passwordConfirmation: "",
-    // };
+    const initialValues = {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        passwordConfirmation: "",
+    };
 
-    // const validationSchema = Yup.object().shape({
-    //     firstName: Yup.string().min(1).max(30).required("You must input a first name"),
-    //     lastName: Yup.string().min(1).max(30).required("You must input a last name"),
-    //     email: Yup.string().min(3).max(15).required("You must input a email"),
-    //     password: Yup.string().min(4).max(20).required("You must input a password"),
-    //     passwordConfirmation: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match')
-    // });
+    const validationSchema = Yup.object().shape({
+        firstName: Yup.string().min(1).max(30).required("You must input a first name"),
+        lastName: Yup.string().min(1).max(30).required("You must input a last name"),
+        email: Yup.string().min(3).max(15).required("You must input a email"),
+        password: Yup.string().min(4).max(20).required("You must input a password"),
+        passwordConfirmation: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match')
+    });
 
-    const handleSubmit = (event) => {
+    const onSubmit = (event) => {
         event.preventDefault();
         const form = new FormData(event.currentTarget);
         let data = {
@@ -85,76 +86,63 @@ function Registration() {
                         <Typography component="h1" variant="h5">
                             Sign up
                         </Typography>
-                        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-                            <Grid container spacing={2}>
-                                <Grid item xs={12} sm={6}>
-                                    <TextField
-                                        className="bg-light"
-                                        autoComplete="given-name"
-                                        name="firstName"
-                                        required
+                        <Box sx={{ mt: 3 }}>
+                            <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+                                <Form >
+                                    <Grid container spacing={2}>
+                                        <Grid item xs={12} sm={6}>
+                                            <label>First Name: </label>
+                                            <Field
+                                                className="form-control"
+                                                name="firstName"
+                                            />
+                                            <ErrorMessage name="firstName" component="span" />
+                                        </Grid>
+                                        <Grid item xs={12} sm={6}>
+                                        <label>Last Name: </label>
+                                            <Field
+                                                className="form-control"
+                                                name="lastName"
+                                            />
+                                            <ErrorMessage name="lastName" component="span" />
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                        <label>Email: </label>
+                                            <Field
+                                                className="form-control"
+                                                name="email"
+                                            />
+                                            <ErrorMessage name="email" component="span" />
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                        <label>Password: </label>
+                                            <Field
+                                                className="form-control"
+                                                name="password"
+                                            />
+                                            <ErrorMessage name="password" component="span" />
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            
+                                        </Grid>
+                                    </Grid>
+                                    <Button
+                                        type="submit"
                                         fullWidth
-                                        id="firstName"
-                                        label="First Name"
-                                        autoFocus
-                                    />
-                                </Grid>
-                                <Grid item xs={12} sm={6}>
-                                    <TextField
-                                        className="bg-light"
-                                        required
-                                        fullWidth
-                                        id="lastName"
-                                        label="Last Name"
-                                        name="lastName"
-                                        autoComplete="family-name"
-                                    />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        className="bg-light"
-                                        required
-                                        fullWidth
-                                        id="email"
-                                        label="Email Address"
-                                        name="email"
-                                        autoComplete="email"
-                                    />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        className="bg-light"
-                                        required
-                                        fullWidth
-                                        name="password"
-                                        label="Password"
-                                        type="password"
-                                        id="password"
-                                        autoComplete="new-password"
-                                    />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <FormControlLabel
-                                        control={<Checkbox value="allowExtraEmails" color="primary" />}
-                                        label="I want to receive inspiration, marketing promotions and updates via email."
-                                    />
-                                </Grid>
-                            </Grid>
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                sx={{ mt: 3, mb: 2 }}
-                            >
-                                Sign Up
-                            </Button>
-                            <Grid container justifyContent="flex-end">
-                                <Grid item>
-                                    <Link href="/login" variant="body2">
-                                        Already have an account? Sign in
-                                    </Link>
-                                </Grid>
-                            </Grid>
+                                        variant="contained"
+                                        sx={{ mt: 3, mb: 2 }}
+                                    >
+                                        Sign Up
+                                    </Button>
+                                    <Grid container justifyContent="flex-end">
+                                        <Grid item>
+                                            <Link href="/login" variant="body2">
+                                                Already have an account? Sign in
+                                            </Link>
+                                        </Grid>
+                                    </Grid>
+                                </Form>
+                            </Formik>
                         </Box>
                     </Box>
                     <Copyright sx={{ mt: 5 }} />
