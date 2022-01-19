701ddf336486781c5d7985244862d2f19e58fbd0
Tyler <TylerDSmalley@gmail.com>
2022-01-18 23:02:29 -0500

    Update UserList.js
    added key to user map iterations to satisfy react complaints

--- client/src/pages/UserList.js
+++ client/src/pages/UserList.js
@@ -1,39 +1,39 @@
 import React from 'react';
 import axios from 'axios';
-import {useEffect,useState} from 'react';
-import {useNavigate} from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function UserList(){
+function UserList() {
 
-    const [listOfUsers,setListOfUsers] = useState([]);
-    let navigate = useNavigate();
+  const [listOfUsers, setListOfUsers] = useState([]);
+  let navigate = useNavigate();
 
-  useEffect(()=>{
-    axios.get("http://localhost:3001/admin/users/list").then((response)=>{
-     setListOfUsers(response.data);
+  useEffect(() => {
+    axios.get("http://localhost:3001/admin/users/list").then((response) => {
+      setListOfUsers(response.data);
     });
-  },[]);
+  }, []);
 
-    return(
-        <div>
-            <h5>List of Users</h5>
-            {listOfUsers.map((value,key)=>{
+  return (
+    <div>
+      <h5>List of Users</h5>
+      {listOfUsers.map((value, key) => {
         return (
-        <div className="card m-2">
-          <div className="card-body">
-          <p className="card-title"><b>Full Name:</b>&nbsp;{value.firstName}&nbsp;{value.lastName}</p>
-          <p className="card-text"><b>Email:</b>&nbsp;{value.email}</p>
-          <p className="cart-text"><b>Role:</b>&nbsp;{value.role}</p>
-          <p className="cart-text"><b>isLocal:</b>&nbsp;{value.isLocal}</p>
-          <p className="cart-text"><b>Account Status:</b>&nbsp;{value.status}</p>
-          <button type="button" className="btn btn-primary" onClick={()=>{navigate(`/admin/list`)}}>Edit</button>
-          <button type="button" className="btn btn-primary" onClick={()=>{navigate(`/admin/delete/${value.id}`)}}>Delete</button>
-          </div>
+          <div key={key} className="card m-2">
+            <div className="card-body">
+              <p className="card-title"><b>Full Name:</b>&nbsp;{value.firstName}&nbsp;{value.lastName}</p>
+              <p className="card-text"><b>Email:</b>&nbsp;{value.email}</p>
+              <p className="cart-text"><b>Role:</b>&nbsp;{value.role}</p>
+              <p className="cart-text"><b>isLocal:</b>&nbsp;{value.isLocal}</p>
+              <p className="cart-text"><b>Account Status:</b>&nbsp;{value.status}</p>
+              <button type="button" className="btn btn-primary" onClick={() => { navigate(`/admin/list`) }}>Edit</button>
+              <button type="button" className="btn btn-primary" onClick={() => { navigate(`/admin/delete/${value.id}`) }}>Delete</button>
+            </div>
           </div>
         );
-        })}
-        </div>
-    )
+      })}
+    </div>
+  )
 }
 
 export default UserList
