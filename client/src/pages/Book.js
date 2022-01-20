90ceb9e337827bc4621f5dd98a3a75478be0b0cf
Tyler <TylerDSmalley@gmail.com>
2022-01-20 00:18:30 -0500

    Update Book.js
    book cleanup

--- client/src/pages/Book.js
+++ client/src/pages/Book.js
@@ -1,12 +1,10 @@
 import React, { useEffect, useState, useContext } from "react";
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
@@ -104,7 +102,7 @@ function Book() {
                                             By: {bookObject.author}
                                         </Typography>
                                         <Typography align="left" component="div">
-                                            <Rating align="left" sx={{ pl: 0, ml: 0 }} name="read-only" value={ratingValue} readOnly />
+                                            <Rating  sx={{ pl: 0, ml: 0 }} name="read-only" value={ratingValue} readOnly /> {ratingValue}
                                         </Typography>
                                         <Typography align="left" variant="subtitle1" color="text.secondary" component="div">
                                             {bookObject.summary}
