2dc0101f93ce24e8772f4a295d939d67520c555d
Tyler <TylerDSmalley@gmail.com>
2022-01-20 16:05:18 -0500

    Update Bookshelves.js
    -added bookshelves to routes.
    -added logic to handle get and post requests

---
+++ server/routes/Bookshelves.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const router = express.Router();
+const { Bookshelves, Books } = require("../models");
+const { validateToken } = require("../middlewares/AuthMiddleware");
+
+
+//Bookshelves ROUTES-------------------------
+
+//add book to shelf
+router.post("/", validateToken, async (req, res) => {
+    console.log(req.body);
+    const {shelf, BookId, UserId} = req.body;
+    await Bookshelves.create({
+        shelf: shelf,
+        BookId: BookId,
+        UserId: UserId,
+    });
+    res.json("BOOK ADDED TO SHELF");
+});
+
+//get all Bookshelves by id
+router.get("/:id", async (req, res) => {
+    const UserId = req.params.id;
+    console.log(UserId)
+    const listOfBookshelves = await Bookshelves.findAll({
+        where: {
+            UserId: UserId, 
+        },
+        include: [Books]
+    });
+    res.json(listOfBookshelves);
+});
+
+
+// //update book by id
+// router.put("/update/:bookId", async (req, res) => {
+//     await Bookshelves.update({
+//         title: req.body.title,
+//         author: req.body.author,
+//         summary: req.body.summary,
+//         genre: req.body.genre,
+//         datePublished: req.body.datePublished,
+//         publisher: req.body.publisher,
+//         isbn: req.body.isbn,
+//         coverPhoto: req.body.coverPhoto,
+//     }, { where: {id : req.body.id} });
+//     res.json(req.body);
+// });
+
+// //delete book by id
+// router.delete("delete/:bookId", validateToken, async (req, res) => {
+//     const bookId = req.params.bookId;
+//     await Bookshelves.destroy({
+//         where: {
+//             id: bookId,
+//         },
+//     });
+//     res.json("BOOK DELETED");
+// })
+
+module.exports = router;
