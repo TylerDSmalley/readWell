6ce044afc9fc830dcbf01a4119326d8014f48a31
Tyler <TylerDSmalley@gmail.com>
2022-01-21 10:39:23 -0500

    Update Review.js
    fixed review route for post

--- server/routes/Review.js
+++ server/routes/Review.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { Bookshelves, Books } = require("../models");
+const { Bookshelves, Books, Reviews } = require("../models");
 const { validateToken } = require("../middlewares/AuthMiddleware");
 
 router.post("/", validateToken, async (req, res) => {
     console.log(req.body);
-    await Bookshelves.create({
+    await Reviews.create({
         summary: req.body.summary,
         BookId: req.body.BookId,
         UserId: req.body.UserId,
     });
     res.json(req.body);
 });
+
+module.exports = router;
