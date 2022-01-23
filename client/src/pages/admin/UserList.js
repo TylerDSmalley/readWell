f5c51b130477d437c1969db89bcbc8d2e594ab91
Tyler <TylerDSmalley@gmail.com>
2022-01-22 22:09:32 -0500

    Update UserList.js

--- client/src/pages/admin/UserList.js
+++ client/src/pages/admin/UserList.js
@@ -11,6 +11,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import TablePagination from '@mui/material/TablePagination';
+import Button from '@mui/material/Button';
 
 
 
@@ -34,69 +35,74 @@ function UserList() {
     });
   };
 
-  function createData(firstName, lastName,email, role, isLocal, status) {
-    return { firstName, lastName,email, role, isLocal, status };
+  function createData(firstName, lastName, email, role, isLocal, status) {
+    return { firstName, lastName, email, role, isLocal, status };
   }
-  
+
 
   const rows = listOfUsers.map((value) => (
-    createData(value.firstName, value.lastName,value.email, value.role, value.isLocal, value.status)
-))
+    createData(value.firstName, value.lastName, value.email, value.role, value.isLocal, value.status)
+  ))
 
-const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
 
-	const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-    };
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
 
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0);
-    };
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   return (
     <TableContainer component={Paper}>
-    <Table sx={{ minWidth: 650 }} aria-label="simple table">
-      <TableHead>
-        <TableRow>
-          <TableCell align="right">First Name</TableCell>
-          <TableCell align="right">Last Name</TableCell>
-          <TableCell align="right">Email</TableCell>
-          <TableCell align="right">Role</TableCell>
-          <TableCell align="right">isLocal</TableCell>
-          <TableCell align="right">Account Status</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {rows.map((row) => (
-          <TableRow
-            key={row.id}
-            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-          >
-            
-            <TableCell align="right">{row.firstName}</TableCell>
-            <TableCell align="right">{row.lastName}</TableCell>
-            <TableCell align="right">{row.email}</TableCell>
-            <TableCell align="right">{row.role}</TableCell>
-            <TableCell align="right">{row.isLocal}</TableCell>
-            <TableCell align="right">{row.status}</TableCell>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="right">First Name</TableCell>
+            <TableCell align="right">Last Name</TableCell>
+            <TableCell align="right">Email</TableCell>
+            <TableCell align="right">Role</TableCell>
+            <TableCell align="right">isLocal</TableCell>
+            <TableCell align="right">Account Status</TableCell>
+            <TableCell align="right">Actions</TableCell>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-    <TablePagination
-                                        rowsPerPageOptions={[5, 10, 25]}
-                                        component="div"
-                                        count={rows.length}
-                                        rowsPerPage={rowsPerPage}
-                                        page={page}
-                                        onPageChange={handleChangePage}
-                                        onRowsPerPageChange={handleChangeRowsPerPage}
-                                    ></TablePagination>
-  </TableContainer>
- 
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+
+              <TableCell align="right">{row.firstName}</TableCell>
+              <TableCell align="right">{row.lastName}</TableCell>
+              <TableCell align="right">{row.email}</TableCell>
+              <TableCell align="right">{row.role}</TableCell>
+              <TableCell align="right">{row.isLocal}</TableCell>
+              <TableCell align="right">{row.status}</TableCell>
+              <TableCell align="right">
+                <Button className="bg-secondary text-white mx-2" onClick={() => {deleteUser(row.id)}}>Edit</Button>
+                <Button className="bg-danger text-white" onClick={() => {deleteUser(row.id)}}>Delete</Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      ></TablePagination>
+    </TableContainer>
+
   )
 }
 
