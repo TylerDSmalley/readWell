192af063df52dfe83c98ddfda4e8809f6e83f1f6
Ileana528 <86622807+Ileana528@users.noreply.github.com>
2022-01-23 13:06:12 -0500

    Update ReviewList.js
    Added table and pagination to Review List

--- client/src/pages/admin/ReviewList.js
+++ client/src/pages/admin/ReviewList.js
@@ -6,6 +6,16 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+//MUI
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import TablePagination from '@mui/material/TablePagination';
+import Button from '@mui/material/Button';
 
 function ReviewList() {
 
@@ -27,40 +37,69 @@ function ReviewList() {
     });
   };
 
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <main className="w-100">
-        <Container sx={{ minHeight: "100vh" }} maxWidth="md">
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              p: 8,
-              my: 5
-            }}
-            justifyContent="center"
-            className='contentBox rounded-3'
+  function createData(id,summary, booktitle, reviewby) {
+    return { id,summary, booktitle, reviewby };
+  }
 
-          >
-            <h5>List of Book Reviews</h5>
-            {listOfReviews.map((value, key) => {
-              return (
-                <div key={key} className="card m-2">
-                  <div className="card-body">
-                    <p className="card-title"><b>Book:</b>&nbsp;{value.Book.title}</p>
-                    <p className="card-title"><b>Review:</b>&nbsp;{value.summary}</p>
-                    <p className="card-title"><b>Review By:</b>&nbsp;{value.User.email}</p>
-                    <button type="button" className="btn btn-primary" onClick={() => { deleteReview(value.id) }}>Delete</button>
-                  </div>
-                </div>
-              );
-            })}
-          </Box>
-        </Container>
-      </main>
-    </ThemeProvider>
+
+  const rows = listOfReviews.map((value) => (
+    createData(value.id, value.summary, value.Book.title, value.User.email)
+  ))
+
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <caption>List of Reviews</caption>
+        <TableHead>
+          <TableRow>
+            <TableCell align="right">ID</TableCell>
+            <TableCell align="right">Book Title</TableCell>
+            <TableCell align="right">Book Review</TableCell>
+            <TableCell align="right">Review By</TableCell>
+            <TableCell align="right">Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
+            <TableRow
+              key={row.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell align="right">{row.id}</TableCell>
+              <TableCell align="right">{row.booktitle}</TableCell>
+              <TableCell align="right">{row.summary}</TableCell>
+              <TableCell align="right">{row.reviewby}</TableCell>
+              <TableCell align="right">
+                <Button className="bg-danger text-white" onClick={() => {deleteReview(row.id)}}>Delete</Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      ></TablePagination>
+    </TableContainer>
+
   )
 }
 
