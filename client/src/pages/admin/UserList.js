c2e14c70a339a6e619688c5df06e0d62a8936c8b
Ileana528 <86622807+Ileana528@users.noreply.github.com>
2022-01-22 20:37:28 -0500

    Update UserList.js
    Added mui table and pagination to User list on admin side

--- client/src/pages/admin/UserList.js
+++ client/src/pages/admin/UserList.js
@@ -2,6 +2,18 @@ import React from 'react';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+//MUI
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import TablePagination from '@mui/material/TablePagination';
+
+
+
 
 function UserList() {
 
@@ -22,25 +34,69 @@ function UserList() {
     });
   };
 
+  function createData(firstName, lastName,email, role, isLocal, status) {
+    return { firstName, lastName,email, role, isLocal, status };
+  }
+  
+
+  const rows = listOfUsers.map((value) => (
+    createData(value.firstName, value.lastName,value.email, value.role, value.isLocal, value.status)
+))
+
+const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+
+	const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
   return (
-    <div className='vh-100'>
-      <h5 className='contentBox mt-3 p-2 rounded'>List of Users</h5>
-      {listOfUsers.map((value, key) => {
-        return (
-          <div key={key} className="card m-2">
-            <div className="card-body">
-              <p className="card-title"><b>Full Name:</b>&nbsp;{value.firstName}&nbsp;{value.lastName}</p>
-              <p className="card-text"><b>Email:</b>&nbsp;{value.email}</p>
-              <p className="cart-text"><b>Role:</b>&nbsp;{value.role}</p>
-              <p className="cart-text"><b>isLocal:</b>&nbsp;{value.isLocal}</p>
-              <p className="cart-text"><b>Account Status:</b>&nbsp;{value.status}</p>
-              <button type="button" className="btn btn-primary" onClick={() => { navigate(`/admin/list`) }}>Edit</button>
-              <button type="button" className="btn btn-primary" onClick={() => {deleteUser(value.id)}}>Delete</button>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+    <TableContainer component={Paper}>
+    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <TableHead>
+        <TableRow>
+          <TableCell align="right">First Name</TableCell>
+          <TableCell align="right">Last Name</TableCell>
+          <TableCell align="right">Email</TableCell>
+          <TableCell align="right">Role</TableCell>
+          <TableCell align="right">isLocal</TableCell>
+          <TableCell align="right">Account Status</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {rows.map((row) => (
+          <TableRow
+            key={row.id}
+            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+          >
+            
+            <TableCell align="right">{row.firstName}</TableCell>
+            <TableCell align="right">{row.lastName}</TableCell>
+            <TableCell align="right">{row.email}</TableCell>
+            <TableCell align="right">{row.role}</TableCell>
+            <TableCell align="right">{row.isLocal}</TableCell>
+            <TableCell align="right">{row.status}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+    <TablePagination
+                                        rowsPerPageOptions={[5, 10, 25]}
+                                        component="div"
+                                        count={rows.length}
+                                        rowsPerPage={rowsPerPage}
+                                        page={page}
+                                        onPageChange={handleChangePage}
+                                        onRowsPerPageChange={handleChangeRowsPerPage}
+                                    ></TablePagination>
+  </TableContainer>
+ 
   )
 }
 
